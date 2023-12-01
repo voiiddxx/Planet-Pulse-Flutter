2ebc597e3034e58f8f08058ed6b2989511421ecf
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-12-01 22:51:03 +0530

    Added Splash Screen

--- lib/View/Screens/start/splash.dart
+++ lib/View/Screens/start/splash.dart
@@ -41,7 +41,28 @@ class SplashScreenState extends State<SplashScreen> {
                   DetailText(
                       color: GlobalColor.headingcolor,
                       text:
-                          "Let’s take a small step to give our contribution to the planet so that our upcoming genereation will stay alive and lorem espum whih can save us")
+                          "Let’s take a small step to give our contribution to the planet so that our upcoming genereation will stay alive and lorem espum whih can save us"),
+                  Image.asset(
+                    "assets/images/planet.png",
+                    height: MediaQuery.of(context).size.height * 0.5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 85,
+                        width: 85,
+                        decoration: BoxDecoration(
+                            color: const Color.fromRGBO(51, 51, 51, 1),
+                            borderRadius: BorderRadius.circular(85)),
+                        child: Icon(
+                          Icons.arrow_forward_ios_sharp,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
