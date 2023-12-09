a0a70df8f20730a53bccc450e8d59f9a2fdf8383
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-12-09 17:36:51 +0530

    Implemented api integreation and ui for approve verification

--- lib/View/Screens/verify/approvemain.dart
+++ lib/View/Screens/verify/approvemain.dart
@@ -105,6 +105,174 @@ class _ApprovemainScreenState extends State<ApprovemainScreen> {
                   ),
                   color: Colors.white),
               width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 20, top: 20, bottom: 10),
+                      child: CustomFont(
+                          color: GlobalColor.headingcolor,
+                          text: "Submitted Assignments",
+                          weight: FontWeight.w600,
+                          size: 14),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: questions['ques'].length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: const EdgeInsets.only(bottom: 5),
+                            height: 500,
+                            width: 500,
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 27, 27, 27),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.3,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                questions['ques'][index]
+                                                    ['quesImage']),
+                                            fit: BoxFit.cover),
+                                        borderRadius: BorderRadius.circular(5)),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  Container(
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                        color: const Color.fromARGB(
+                                            143, 62, 62, 62),
+                                        borderRadius: BorderRadius.circular(7)),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(15),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              const Icon(
+                                                Icons.animation_sharp,
+                                                color: Colors.yellow,
+                                                size: 15,
+                                              ),
+                                              const SizedBox(
+                                                width: 10,
+                                              ),
+                                              CustomFont(
+                                                  color: const Color.fromARGB(
+                                                      255, 255, 255, 255),
+                                                  text: questions['ques'][index]
+                                                      ['question'],
+                                                  weight: FontWeight.w600,
+                                                  size: 15),
+                                            ],
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          const Row(
+                                            children: [
+                                              Icon(
+                                                Icons.location_on_outlined,
+                                                color: Color.fromARGB(
+                                                    255, 59, 216, 255),
+                                                size: 15,
+                                              ),
+                                              SizedBox(
+                                                width: 10,
+                                              ),
+                                              SizedBox(
+                                                width: 250,
+                                                child: CustomFont(
+                                                    color: Color.fromARGB(
+                                                        255, 219, 219, 219),
+                                                    text:
+                                                        "Baba Farid college of engineering and technology",
+                                                    weight: FontWeight.w300,
+                                                    size: 13),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(
+                                            height: 15,
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              SizedBox(
+                                                  height: 50,
+                                                  width: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.37,
+                                                  child: ElevatedButton(
+                                                      style: ElevatedButton.styleFrom(
+                                                          shape: ContinuousRectangleBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          10))),
+                                                      onPressed: () {},
+                                                      child: CustomFont(
+                                                          color: GlobalColor
+                                                              .headingcolor,
+                                                          text: "Approve",
+                                                          weight:
+                                                              FontWeight.w400,
+                                                          size: 12))),
+                                              SizedBox(
+                                                  height: 50,
+                                                  width: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.37,
+                                                  child: OutlinedButton(
+                                                      style: OutlinedButton.styleFrom(
+                                                          shape: ContinuousRectangleBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          10))),
+                                                      onPressed: () {},
+                                                      child: const CustomFont(
+                                                          color: Colors.white,
+                                                          text: "Approve",
+                                                          weight:
+                                                              FontWeight.w400,
+                                                          size: 12)))
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ));
